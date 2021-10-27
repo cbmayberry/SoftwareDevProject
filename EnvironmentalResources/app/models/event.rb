062f5e45c198fd2bec9fb5c6ab6012cbdac5ff5f
Caroline Mayberry <cmayberry@tulane.edu>
2021-10-27 04:02:24 +0000

    Started setting up validations in event.rb and org.rb models

--- EnvironmentalResources/app/models/event.rb
+++ EnvironmentalResources/app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ActiveRecord::Base
     belongs_to :org
+    
+#validations
+    #event only valid if associated w an organization
+    validates :org, :presence => true
+    
 end
--- EnvironmentalResources/app/models/org.rb
+++ EnvironmentalResources/app/models/org.rb
@@ -1,3 +1,4 @@
 class Org < ActiveRecord::Base
-    has_many :events
+    has_many :events, dependent: :destroy #org's events destroyed if org destroyed
+
 end
