812e6cab90895f0391f1fb14040cd41637c08ff7
Oliver Canosa <ocanosa@tulane.edu>
2021-11-01 20:00:46 +0000

    changed navbar routes for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
-  get 'calendar/show'
   resources :orgs, :events
   root :to => 'welcome#index'
-end
-
-CalendarExample::Application.routes.draw do
-  resource :calendar, only: [:show], controller: :calendar
-  root to: "calendar#show"
-end
-
-Rails.application.routes.draw do
-    direct(:orgs) { "https://peaceful-inlet-32598.herokuapp.com/orgs" }
-    direct(:events) { "https://peaceful-inlet-32598.herokuapp.com/events" }
+  get 'orgs', to: 'welcome#orgs'
+  get 'events', to: 'welcome#events'
 end
