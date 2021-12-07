e3a0ab65eb43e12ff834afea5cd280d5101e84b4
Caroline Mayberry <cmayberry@tulane.edu>
2021-12-07 09:38:28 -0600

    test fix for deployment issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+     has_and_belongs_to_many :orgs
      # adds virtual attributes for authentication
      has_secure_password
      # validates email
      validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
+
+
 end
