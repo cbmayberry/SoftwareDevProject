15b7002a2a5386dffdfbed11da604286c5c8b221
Oliver Canosa <ocanosa@tulane.edu>
2021-12-07 05:24:43 +0000

    user login fixes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,20 @@
 class SessionsController < ApplicationController
-    def new; end
-    def create
-      user = User.find_by(email: params[:email])
-      # finds existing user, checks to see if user can be authenticated
-      if user.present? && user.authenticate(params[:password])
-      # sets up user.id sessions
-        session[:user_id] = user.id
-        redirect_to root_path, notice: 'Logged in successfully'
-      else
-        flash.now[:alert] = 'Invalid email or password'
-        render :new
-      end
+  def new
+  end
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    # finds existing user, checks to see if user can be authenticated
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to root_path
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
     end
-    def destroy
-      # deletes user session
-      session[:user_id] = nil
-      redirect_to root_path, notice: 'Logged Out'
-    end
+  end
+  def destroy
+    # deletes user session
+    log_out
+    redirect_to root_path, notice: 'Logged Out'
+  end
 end
