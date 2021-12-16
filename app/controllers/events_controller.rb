6a1c0a833b84b0cf3387aee4c0a8cb38e018d141
Caroline Mayberry <cmayberry@tulane.edu>
2021-12-16 06:12:30 -0600

    fix event controller bug

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -20,7 +20,7 @@ class EventsController < ApplicationController
 
   # GET /events/1 or /events/1.json
   def show
-    @event = Event.find(params[:id])
+    #@event = Event.find(params[:id])
   end
 
   # GET /events/new
